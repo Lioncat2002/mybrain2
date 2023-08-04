5ce8b3b7af0868bb6d3d77aa2ce9c8f9c3d03ff9
lioncat2002 <sankhayan2002@gmail.com>
2023-08-04 16:45:00 +0530

    feat: added clickable cards

---
+++ backend/uploader.py
@@ -0,0 +1,47 @@
+import weaviate
+import json
+from dotenv import load_dotenv
+from fastapi import FastAPI
+import os
+load_dotenv(".env")
+client=weaviate.Client(
+    url=os.getenv("WEAVIATE_URL"),
+    auth_client_secret=weaviate.AuthApiKey(api_key=os.getenv("WEAVIATE_APIKEY")),
+    additional_headers={
+        "X-HuggingFace-Api-Key":os.getenv("HUGGINGFACE_APIKEY")
+    }
+)
+
+classobj={
+    "class":"Result",
+    "vectorizer":"text2vec-huggingface",
+    "moduleConfig":{
+        "text2vec-huggingface":{
+            "model":"sentence-transformers/all-MiniLM-L6-v2",
+            "options":{
+                "waitForModel":True
+            }
+        }
+    }
+}
+
+
+
+client.schema.create_class(classobj)
+f=open("BrowserHistory.json")
+data=json.load(f)
+f.close()
+data=data["Browser History"]
+
+with client.batch(batch_size=100) as batch:
+        for i,d in enumerate(data):
+            print("importing data: ",i+1)
+            properties={
+                "title":d["title"],
+                "url":d["url"],
+                "time":d["time_usec"]
+            }
+            client.batch.add_data_object(
+                properties,
+                "Result"
+            )
--- lib/components/result_card.dart
+++ lib/components/result_card.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class ResultCard extends StatelessWidget {
   final String title;
@@ -11,34 +12,42 @@ class ResultCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      child: SizedBox(
-        width: 300,
-        height: 100,
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                title,
-                style: const TextStyle(fontSize: 20),
-              ),
-              const Divider(
-                color: Colors.black, //color of divider
-                height: 1, //height spacing of divider
-                thickness: 1, //thickness of divier line
-                indent: 0, //spacing at the start of divider
-                endIndent: 0, //spacing at the end of divider
-              ),
-              Text(
-                url,
-                style: const TextStyle(
-                    color: Color.fromARGB(255, 80, 80, 80),
-                    fontSize: 12,
-                    overflow: TextOverflow.ellipsis),
-              )
-            ],
+    return GestureDetector(
+      onTap: () async {
+        final Uri _url = Uri.parse(url);
+        if (!await launchUrl(_url)) {
+          throw Exception("Couldn't launch $url");
+        }
+      },
+      child: Card(
+        child: SizedBox(
+          width: 300,
+          height: 100,
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  title,
+                  style: const TextStyle(fontSize: 20),
+                ),
+                const Divider(
+                  color: Colors.black, //color of divider
+                  height: 1, //height spacing of divider
+                  thickness: 1, //thickness of divier line
+                  indent: 0, //spacing at the start of divider
+                  endIndent: 0, //spacing at the end of divider
+                ),
+                Text(
+                  url,
+                  style: const TextStyle(
+                      color: Color.fromARGB(255, 80, 80, 80),
+                      fontSize: 12,
+                      overflow: TextOverflow.ellipsis),
+                )
+              ],
+            ),
           ),
         ),
       ),
